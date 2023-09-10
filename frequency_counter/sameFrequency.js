624a1d48bc1e634dbc50f9b215bfec91421141ee
unhyif <ang21icjh@gmail.com>
2023-09-10 15:56:46 +0900

    frequency counter: sameFrequency

--- frequency_counter/sameFrequency.js
+++ frequency_counter/sameFrequency.js
@@ -1,24 +1,17 @@
-// O(n log n)
-function validAnagram1(str1, str2) {
-  str1 = str1.split("");
-  str2 = str2.split("");
-  str1.sort();
-  str2.sort();
-  return str1.join("") === str2.join("");
-}
-
 // O(n)
-function validAnagram2(str1, str2) {
+function sameFrequency(num1, num2) {
+  const str1 = String(num1);
+  const str2 = String(num2);
   if (str1.length !== str2.length) return false;
+
   const count = {};
 
   for (const str of str1) {
     count[str] = (count[str] || 0) + 1;
   }
-
   for (const str of str2) {
     if (!count[str]) return false;
-    count[str] -= 1;
+    count[str]--;
   }
 
   return true;
--- multiple_pointer/countUniqueValues.js
+++ multiple_pointer/countUniqueValues.js
@@ -1,41 +1,41 @@
 function countUniqueValues1(arr) {
-    const set = new Set(arr);
-    return set.size;
+  const set = new Set(arr);
+  return set.size;
 }
 
 function countUniqueValues2(arr) {
-    const length = arr.length;
-    if (length < 2) return length;
+  const length = arr.length;
+  if (length < 2) return length;
 
-    let count = 1;
-    let backIndex = 0;
-    let frontIndex = 1;
+  let count = 1;
+  let backIndex = 0;
+  let frontIndex = 1;
 
-    while (frontIndex < length) {
-        if (arr[backIndex] !== arr[frontIndex]) {
-            count++;
-        }
-        backIndex++;
-        frontIndex++;
+  while (frontIndex < length) {
+    if (arr[backIndex] !== arr[frontIndex]) {
+      count++;
     }
+    backIndex++;
+    frontIndex++;
+  }
 
-    return count;
+  return count;
 }
 
 function countUniqueValues3(arr) {
-    const length = arr.length;
-    if (length < 2) return length;
+  const length = arr.length;
+  if (length < 2) return length;
 
-    let backIndex = 0;
-    let frontIndex = 1;
+  let backIndex = 0;
+  let frontIndex = 1;
 
-    while (frontIndex < length) {
-        if (arr[backIndex] !== arr[frontIndex]) {
-            backIndex++;
-            arr[backIndex] = arr[frontIndex];
-        }
-        frontIndex++;
+  while (frontIndex < length) {
+    if (arr[backIndex] !== arr[frontIndex]) {
+      backIndex++;
+      arr[backIndex] = arr[frontIndex];
     }
+    frontIndex++;
+  }
 
-    return backIndex + 1;
+  return backIndex + 1;
 }
