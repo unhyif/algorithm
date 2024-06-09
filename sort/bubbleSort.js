03ca72c39b62a6cab1a117e5e619cc4eef9dbe23
unhyif <ang21icjh@gmail.com>
2024-06-09 23:22:47 +0900

    sort: bubbleSort optimization

--- sort/bubbleSort.js
+++ sort/bubbleSort.js
@@ -1,12 +1,18 @@
 function bubbleSort(arr) {
   for (let i = arr.length; 0 < i; i--) {
+    let swapped = false;
+
     for (let j = 0; j < i - 1; j++) {
       if (arr[j] > arr[j + 1]) {
         const greater = arr[j];
         arr[j] = arr[j + 1];
         arr[j + 1] = greater;
+
+        swapped = true;
       }
     }
+
+    if (!swapped) break;
   }
 
   return arr;
