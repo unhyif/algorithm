1761bfa3ec578bbed49141827124c525df4f1ba3
unhyif <ang21icjh@gmail.com>
2024-06-10 17:24:39 +0900

    sort: insertionSort

---
+++ sort/insertionSort.js
@@ -0,0 +1,13 @@
+function insertionSort(arr) {
+  for (let i = 1; i < arr.length; i++) {
+    const currentValue = arr[i];
+
+    let j;
+    for (j = i - 1; 0 <= j && currentValue < arr[j]; j--) {
+      arr[j + 1] = arr[j];
+    }
+    arr[j + 1] = currentValue;
+  }
+
+  return arr;
+}
