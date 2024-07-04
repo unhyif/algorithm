29df914db9385f07e73b045127dfc0df01257e64
unhyif <ang21icjh@gmail.com>
2024-07-04 22:21:03 +0900

    DoublyLinkedList: push, pop

---
+++ linked_list/DoublyLinkedList.js
@@ -0,0 +1,50 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+    this.prev = null;
+  }
+}
+
+class DoublyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  push(value) {
+    const node = new Node(value);
+
+    if (!this.length) {
+      this.head = node;
+      this.tail = node;
+    } else {
+      this.tail.next = node;
+      node.prev = this.tail;
+      this.tail = node;
+    }
+
+    this.length++;
+    return this;
+  }
+
+  pop() {
+    if (!this.length) return undefined;
+
+    const node = this.tail;
+
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.tail = node.prev;
+      this.tail.next = null;
+
+      node.prev = null;
+    }
+
+    this.length--;
+    return node;
+  }
+}
