5c111b4aace8fecf020ca2193afd5dbcfb058c51
unhyif <ang21icjh@gmail.com>
2024-09-27 01:21:04 +0900

    BinarySearchTree: find

--- tree/BinarySearchTree.js
+++ tree/BinarySearchTree.js
@@ -30,6 +30,23 @@ class BinarySearchTree {
       }
     }
   }
+
+  find(value) {
+    if (!this.root) return false;
+
+    let current = this.root;
+    while (current) {
+      if (value < current.value) {
+        current = current.left;
+      } else if (value > current.value) {
+        current = current.right;
+      } else {
+        return true;
+      }
+    }
+
+    return false;
+  }
 }
 
 class Node {
