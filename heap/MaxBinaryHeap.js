db64ad96de84f47a42d788c6d35838058803b742
unhyif <ang21icjh@gmail.com>
2024-10-20 20:13:53 +0900

    MaxBinaryHeap: extractMax

--- heap/MaxBinaryHeap.js
+++ heap/MaxBinaryHeap.js
@@ -21,4 +21,45 @@ class MaxBinaryHeap {
       this.bubbleUp(parentIndex);
     }
   }
+
+  extractMax() {
+    const max = this.values[0];
+    const end = this.values.pop();
+    if (this.values.length > 0) {
+      this.values[0] = end;
+      this.sinkDown(0);
+    }
+    return max;
+  }
+
+  sinkDown(index) {
+    const value = this.values[index];
+    const leftChildIndex = 2 * index + 1;
+    const rightChildIndex = 2 * index + 2;
+    const leftChildValue = this.values[leftChildIndex];
+    const rightChildValue = this.values[rightChildIndex];
+    const length = this.values.length;
+    let swapIndex = null;
+
+    if (leftChildIndex < length) {
+      if (leftChildValue > value) {
+        swapIndex = leftChildIndex;
+      }
+    }
+
+    if (rightChildIndex < length) {
+      if (
+        (swapIndex === null && rightChildValue > value) ||
+        (swapIndex !== null && rightChildValue > leftChildValue)
+      ) {
+        swapIndex = rightChildIndex;
+      }
+    }
+
+    if (swapIndex !== null) {
+      this.values[index] = this.values[swapIndex];
+      this.values[swapIndex] = value;
+      this.sinkDown(swapIndex);
+    }
+  }
 }
